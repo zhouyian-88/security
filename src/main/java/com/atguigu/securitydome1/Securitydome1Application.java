000f606d2ed132704a232162f25085498eaba82d
yianzhou <2873642764@qq.com>
2022-11-21 20:24:46 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/securitydome1/Securitydome1Application.java
@@ -0,0 +1,13 @@
+package com.atguigu.securitydome1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Securitydome1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Securitydome1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/atguigu/securitydome1/Securitydome1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.securitydome1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Securitydome1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
